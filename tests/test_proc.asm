403a1ff8da1116aa9b5242c82e9953a74e5dcc7f
damien.leroux <damien.leroux@c1dc5971-5c3e-0410-84a0-c3311864b1d4>
2009-06-14 12:45:15 +0000

    I believe the bug in procasm that prevented from using both arguments AND locals is fixed. Requires further testing.
    git-svn-id: https://tinyaml.googlecode.com/svn/trunk@97 c1dc5971-5c3e-0410-84a0-c3311864b1d4

--- tests/test_proc.asm
+++ tests/test_proc.asm
@@ -1,27 +1,27 @@
 require "procasm.wc"
 
-#func pouet(a, b)
-#	+$a push " " +$b push "\n" print 4
-#	ret 0
-#endfunc
-#
-#func var(n)
-#	local arg, counter {
-#		push 0 -$counter
-#	_v_lp:	+$counter inc +$n inf [
-#			-$arg
-#			push "arg #" +$counter push " : " +$arg push "\n" print 5
-#			+$counter inc -$counter
-#			jmp @_v_lp
-#		]
-#	}
-#	# returns last argument
-#endfunc
-#
-#func hop()
-#	push "hello"
-#	push "world"
-#endfunc
+func pouet(a, b)
+	+$a push " " +$b push "\n" print 4
+	ret 0
+endfunc
+
+func var(n)
+	local arg, counter {
+		push 0 -$counter
+	_v_lp:	+$counter inc +$n inf [
+			-$arg
+			push "arg #" +$counter push " : " +$arg push "\n" print 5
+			+$counter inc -$counter
+			jmp @_v_lp
+		]
+	}
+	# returns last argument
+endfunc
+
+func hop()
+	push "hello"
+	push "world"
+endfunc
 
 asm
 	local a, b {
@@ -31,8 +31,8 @@ asm
 		local z, t {
 			push "hello" -$t
 			push "hello" -$x
-			#%pouet(+$x, %var(push 3, push "A", push "B", push "world"))
-			#%pouet(%hop())
+			%pouet(+$x, %var(push 3, push "A", push "B", push "world"))
+			%pouet(%hop())
 		}
 	}
 end
